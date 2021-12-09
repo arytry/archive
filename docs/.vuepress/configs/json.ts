30a319e84936a912e52f6bf9775b8810a3e95895
jabar <jabar.wang@silksoftware.com>
2021-12-09 18:34:04 +0800

    Update

--- docs/.vuepress/configs/json.ts
+++ docs/.vuepress/configs/json.ts
@@ -2,6 +2,7 @@ export const json = {
     archive: '归档',
     'algorithms': '算法',
     'algorithms/binarytree': '二叉树',
+    'algorithms/encryption': '加密算法',
     'docker/guide': 'docker指南',
     'docker/reference': 'docker参考',
     'docker/instance': 'docker实例',
